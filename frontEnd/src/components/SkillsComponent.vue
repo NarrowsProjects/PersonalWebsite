cd30866b3771e81f80771efbe4e18683f6963148
backup_laptop <backup_laptop@email>
2026-09-11 05:56:35 -0700

    chore: update skills

--- frontEnd/src/components/SkillsComponent.vue
+++ frontEnd/src/components/SkillsComponent.vue
@@ -8,18 +8,20 @@ export default {
   data(){
     return{
       utilities:[
-        { name: "Dutch", score: 4 },
-        { name: "English", score: 4 },
+        { name: "Dutch (C2)", score: 4.5 },
+        { name: "English (C2)", score: 4.5 },
+        { name: "French (A1-A2)", score: 1 },
+        { name: "Git Version Management", score: 4 },
         { name: "Full Stack Development", score: 4 },
-        { name: "Scrum", score: 3 },
+        { name: "Agile Development", score: 3.5 },
       ] as skill[],
       frameWorks:[
-        { name: "Java", score: 4 },
         { name: "Java/Type - Script", score: 4 },
-        { name: "Vue", score: 3 },
+        { name: "Python", score: 3 },
+        { name: "Vue", score: 4 },
         { name: "SpringBoot", score: 3 },
+        { name: "React", score: 2.5 },
         { name: "Mithriljs", score: 4 },
-        { name: "Ruby on Rails", score: 4 },
       ] as skill[],
     }
   },
@@ -34,23 +36,35 @@ export default {
     <div class="skill-rating" v-for="(utility,index) in utilities" :key="index">
       <span>{{ utility.name }}</span>
       <div class="rating">
-        <template v-for="number in [5,4,3,2,1]" :key="number">
-          <input type="radio" :name="'skill-'+index" disabled :checked="number===utility.score"><label @click="utility.score=2">▄</label>
-        </template>
+        <span
+          v-for="number in [5, 4, 3, 2, 1]"
+          :key="number"
+          :class="{
+            full: utility.score >= number,
+            half: utility.score === number - 0.5
+          }"
+        >▄</span>
       </div>
     </div>
   </article>
 
   <article>
     <h5 class="yellow">Languages & frameworks</h5>
-    <div class="skill-rating" v-for="(framework,index) in frameWorks" :key="index">
-      <span>{{ framework.name }}</span>
-      <div class="rating">
-        <template v-for="score in [5,4,3,2,1]" :key="score">
-          <input type="radio" :name="'skill-'+index+utilities.length" disabled :checked="score===framework.score"><label>▄</label>
-        </template>
-      </div>
-    </div>
+<div class="skill-rating" v-for="(framework, index) in frameWorks" :key="index">
+  <span>{{ framework.name }}</span>
+
+  <div class="rating">
+    <span
+      v-for="number in [5, 4, 3, 2, 1]"
+      :key="number"
+      :class="{
+        full: framework.score >= number,
+        half: framework.score === number - 0.5
+      }"
+    >▄</span>
+  </div>
+</div>
+
   </article>
 
   </div>
@@ -65,4 +79,27 @@ img {
 article{
   margin-left: 10px;
 }
+.rating {
+  display: flex;
+}
+
+.rating span {
+  color: #444;
+}
+
+.rating span.full {
+  color: #FFD700;
+}
+
+.rating span.half {
+  background: linear-gradient(
+    to right,
+    #FFD700 50%,
+    #444 50%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+}
+
 </style>
